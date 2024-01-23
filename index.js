3156804816683d53ac212e1272c4aa6934c92991
Pankaj Kumar <pankaj@cloudtradetech.com>
2024-01-23 14:44:01 +0530

    beautifed the code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-import { Server } from 'socket.io';
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
 // open the database file
 const db = await open({
-    filename: 'chat.db',
-    driver: sqlite3.Database
+  filename: "chat.db",
+  driver: sqlite3.Database,
 });
 
 // create 'message' table
@@ -19,59 +19,62 @@ await db.exec(`
         client_offset TEXT UNIQUE,
         content TEXT
     )
-`)
+`);
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-    connectionStateRecovery: {}
+  connectionStateRecovery: {},
 });
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'))
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-io.on('connection', async (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', async (msg, client_offset, callback) => {
-        let result;
-        try {
-            // store the message in the database
-            result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, client_offset)
-        }
-        catch (e) {
-            if(e.errno === 19 /* SQLITE_CONSTRAINT */) {
-                // the message was already inserted, so we notify the client
-                callback();
-            }
-            else {
-                // nothing to do, let the client retry
-            }
-            return;
-        }
-        // include the offset with the message
-        io.emit('chat message', msg, result.lastID)
-        // acknowledge the event
+io.on("connection", async (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", async (msg, client_offset, callback) => {
+    let result;
+    try {
+      // store the message in the database
+      result = await db.run(
+        "INSERT INTO messages (content, client_offset) VALUES (?, ?)",
+        msg,
+        client_offset
+      );
+    } catch (e) {
+      if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
+        // the message was already inserted, so we notify the client
         callback();
-    })
-    
-    if(!socket.recovered) {
-        // if the connection state recovery was not successful
-        try {
-            await db.each('SELECT id, content FROM messages WHERE id > ?',
-                [socket.handshake.auth.serverOffset || 0],
-                (_err, row) => {
-                    socket.emit('chat message', row.content, row.id);
-                }
-            )
-        } catch (e) {
-            // something went wrong
-        }
+      } else {
+        // nothing to do, let the client retry
+      }
+      return;
     }
-})
+    // include the offset with the message
+    io.emit("chat message", msg, result.lastID);
+    // acknowledge the event
+    callback();
+  });
 
-server.listen(3000, ()=> {
-    console.log('server running at http://localhost:3000');
+  if (!socket.recovered) {
+    // if the connection state recovery was not successful
+    try {
+      await db.each(
+        "SELECT id, content FROM messages WHERE id > ?",
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit("chat message", row.content, row.id);
+        }
+      );
+    } catch (e) {
+      // something went wrong
+    }
+  }
+});
+
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
 });
